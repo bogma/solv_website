e0a72d2769e4ac876023788b5af930f9336071e8
Martin Bogensperger <m.bogensperger@commend.com>
2019-02-11 16:44:28 +0100

    Script for extracting champions

---
+++ scripts/Get-Champs.ps1
@@ -0,0 +1,4 @@
+﻿$relay_fst = Get-ChildItem .\data\championships -Recurse -Filter *.yaml | Select-String -Pattern '- name: \[(.*), .*\]' | %{ $_.Matches.Groups[1].Value }
+$relay_snd = Get-ChildItem .\data\championships -Recurse -Filter *.yaml | Select-String -Pattern '- name: \[.*, (.*)\]' | %{ $_.Matches.Groups[1].Value }
+$single = Get-ChildItem .\data\championships -Recurse -Filter *.yaml | Select-String -Pattern '- name: ([^\[].*)' | %{ $_.Matches.Groups[1].Value }
+
