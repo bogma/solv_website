bbd5c1f572421abc4f6d228d4dfcfe69ce06d87a
bogma <martin.bogensperger@gmail.com>
2019-02-12 17:29:02 +0100

    update todos

--- scripts/Get-Champs.ps1
+++ scripts/Get-Champs.ps1
@@ -2,4 +2,8 @@
 $relay_snd = Get-ChildItem ..\data\championships -Recurse -Filter *.yaml | Select-String -Pattern '- name: \[.*, (.*)\]' | %{ $_.Matches.Groups[1].Value }
 $single = Get-ChildItem ..\data\championships -Recurse -Filter *.yaml | Select-String -Pattern '- name: ([^\[].*)' | %{ $_.Matches.Groups[1].Value }
 
-$relay_fst + $relay_snd + $single | Select-Object -Unique | Sort-Object > out.txt
+
+$existing = Get-ChildItem ..\data\club_members.yaml | Select-String -Pattern '    - (.*)' | %{ $_.Matches.Groups[1].Value }
+$champs = $relay_fst + $relay_snd + $single | Select-Object -Unique | Sort-Object
+
+$champs | Where-Object {!($existing -contains $_)} | Sort-Object > missing_members.txt
